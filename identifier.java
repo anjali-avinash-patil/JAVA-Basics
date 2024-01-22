d9de26ade705d2b968d01eb3f3875acb9eecab83
Anjali Avinash Patil <138760386+anjali-avinash-patil@users.noreply.github.com>
2024-01-22 20:18:35 +0530

    Identifier

---
+++ identifier.java
@@ -0,0 +1,20 @@
+// variables are identified by unique name is called as identifiers.
+// In javaThe general rules for naming variables are:
+
+/*Names can contain letters, digits, underscores, and dollar signs
+Names must begin with a letter
+Names should start with a lowercase letter and it cannot contain whitespace
+Names can also begin with $ and _ (but we will not use it in this tutorial)
+Names are case sensitive ("myVar" and "myvar" are different variables)
+Reserved words (like Java keywords, such as int or boolean) cannot be used as names */
+
+class identifier
+{
+	public static void main(String args[])
+	{
+		int daysofweek = 7;
+		int d = 7;
+		System.out.println(daysofweek);
+		System.out.println(d);
+	}
+}
