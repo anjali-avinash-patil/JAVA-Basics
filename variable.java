70f679be4432757afeb38c0711e46bb710a8b16b
Anjali Avinash Patil <138760386+anjali-avinash-patil@users.noreply.github.com>
2024-01-22 20:10:37 +0530

    Variables
    In this file, I write a program on variables in java

---
+++ variable.java
@@ -0,0 +1,32 @@
+// variables are containers for storing a data values
+/* string
+int
+float
+char
+boolean */
+class variable
+{
+	public static void main(String args[])
+	{
+		// int
+		int a=3 , b=4, c=4;
+		System.out.println(a+b+c);
+		
+		// string
+		
+		String name = "hello anju";
+		System.out.println(name);
+		
+		// float
+		float f = 3.0f;
+		System.out.println(f);
+
+		//char
+		char s = 'A';
+		System.out.println(s);
+
+		// boolean
+		boolean j = true;
+		System.out.println(a);
+	}
+}
