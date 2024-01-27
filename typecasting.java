69f9d9dcff2bf96049b9d5a1500cd7f60e572729
Anjali Avinash Patil <138760386+anjali-avinash-patil@users.noreply.github.com>
2024-01-27 16:23:39 +0530

    Typecasting in java

---
+++ typecasting.java
@@ -0,0 +1,26 @@
+// winding typecastinh
+// smaller to lower datatypes
+
+class typecasting
+{
+	public static void main(String args[])
+	{
+		int a = 12;
+		double b = a;
+		System.out.println(a);
+		System.out.println(b);
+	
+
+
+// narrowing typecasting
+// lower to smaller datatypes
+	
+		double d = 4.5d;
+		int c = (int)d;
+		System.out.println(d);
+		System.out.println(c);
+	}
+}
+		
+
+		
